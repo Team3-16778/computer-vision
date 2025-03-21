472040dee8575457d7741771ca25174abf438a5f
Chase <mcallen2@andrew.cmu.edu>
2025-03-21 18:11:31 -0400

    adding yolo detection

--- camtest.py
+++ camtest.py
@@ -5,7 +5,10 @@ camera_device = '/dev/video0'
 
 # Open the camera
 # cap = cv2.VideoCapture(camera_device, cv2.CAP_V4L2) # V4L2 can only support 8bit bayer, DONT USE!!! https://forums.developer.nvidia.com/t/green-screen-issue-on-nvidia-jetson-nano-and-raspberry-pi-v2-1-camera/220945/7
+# cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv2.CAP_GSTREAMER)
 cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv2.CAP_GSTREAMER)
+# cap = cv2.VideoCapture(0, cv2.CAP_FFMPEG)
+
 # Check if the camera is opened correctly
 if not cap.isOpened():
     print("Error: Camera could not be opened.")
